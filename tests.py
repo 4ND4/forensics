cb09969eb6377836fb2ec1fafd01fc91ece6c032
felix <santiago.anda@outlook.com>
2017-03-03 20:56:15 +0000

    md5 collision detection

--- tests.py
+++ tests.py
@@ -2,7 +2,9 @@ import pytsk3
 import os
 import hashlib
 
-list = []
+listObject = []
+listMD5 = []
+listDuplicate = []
 
 def directoryRecurse(directoryObject, parentPath):
     for entryObject in directoryObject:
@@ -17,37 +19,28 @@ def directoryRecurse(directoryObject, parentPath):
 
         try:
 
-            filepath = '/%s/%s' % ('/'.join(parentPath), entryObject.info.name.name)
+            filePath = '/%s/%s' % ('/'.join(parentPath), entryObject.info.name.name)
 
             if f_type == pytsk3.TSK_FS_META_TYPE_DIR:
                 sub_directory = entryObject.as_directory()
                 parentPath.append(entryObject.info.name.name)
                 directoryRecurse(sub_directory, parentPath)
                 parentPath.pop(-1)
-                print "Directory: %s" % filepath
+                print "Directory: %s" % filePath
 
             elif f_type == pytsk3.TSK_FS_META_TYPE_REG and entryObject.info.meta.size != 0:
 
-                filedata = entryObject.read_random(0, entryObject.info.meta.size)
+                fileData = entryObject.read_random(0, entryObject.info.meta.size)
+
                 md5hash = hashlib.md5()
-                md5hash.update(filedata)
+                md5hash.update(fileData)
                 sha1hash = hashlib.sha1()
-                sha1hash.update(filedata)
+                sha1hash.update(fileData)
+                listObject.append(entryObject)
 
-                list.append(filedata)
+                listMD5.append(md5hash.hexdigest())
 
-                print entryObject.info.name.name
-                print md5hash.hexdigest()
-
-
-            elif f_type == pytsk3.TSK_FS_META_TYPE_REG and entryObject.info.meta.size == 0:
-
-                '''
-                wr.writerow([int(entryObject.info.meta.addr), '/'.join(parentPath) + entryObject.info.name.name,
-                             datetime.datetime.fromtimestamp(entryObject.info.meta.crtime).strftime(
-                                 '%Y-%m-%d %H:%M:%S'), int(entryObject.info.meta.size),
-                             "d41d8cd98f00b204e9800998ecf8427e", "da39a3ee5e6b4b0d3255bfef95601890afd80709"])
-                '''
+            #elif f_type == pytsk3.TSK_FS_META_TYPE_REG and entryObject.info.meta.size == 0:
 
         except IOError as e:
             print e
@@ -59,23 +52,32 @@ url = imageFile.name
 
 dirPath = "/"
 
-# img = pytsk3.Img_Info(url)
-
-# imageFile = r"/././dev/disk2"   # diskutil list  live forensics
-
-# imageHandle = pytsk3.Img_Info(imageFile)
-
 imageHandle = pytsk3.Img_Info(url)
 
 partitionTable = pytsk3.Volume_Info(imageHandle)
 
 for partition in partitionTable:
-    #print partition.addr, partition.desc, "%ss(%s)" % (partition.start, partition.start * 512), partition.len
-
     if 'FAT32' in partition.desc:
         filesystemObject = pytsk3.FS_Info(imageHandle, offset=(partition.start*512))  # absolute offset 512
         directoryObject = filesystemObject.open_dir(path=dirPath)
 
-        #print "Directory: ", dirPath
-
         directoryRecurse(directoryObject, [])
+
+for i in range(len(listMD5)):
+    for j in range(i+1, len(listMD5)):
+
+        if listMD5[i] == listMD5[j]:
+            if i not in listDuplicate:
+                listDuplicate.append(i)
+            if j not in listDuplicate:
+                listDuplicate.append(j)
+
+print 'baby bunny'
+
+outputPath ='~/Documents/UCD/Work/'
+
+for i in listDuplicate:
+    if listObject[i].info.meta.size != 4096:        # find out a better filter for empty directories
+        output = open(listObject[i].info.name.name, "w+")
+        output.write(listObject[i].read_random(0, listObject[i].info.meta.size))
+        output.close
